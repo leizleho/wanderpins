f4dfd5b50835b44c756a0ef0a4bce3bf45217946
leizleho <ho.leizle@gmail.com>
2019-04-18 08:24:17 -0700

    add reducer

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './pages/App';
 import Splash from './pages/Splash';
 import Context from './context';
+import reducer from './reducer';
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import * as serviceWorker from './serviceWorker';
 
 const Root = () => {
   const initialState = useContext(Context);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/login" component={Splash} />
-      </Switch>
+      <Context.Provider value={{ state, dispatch }}>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Splash} />
+        </Switch>
+      </Context.Provider>
     </Router>
   );
 };
