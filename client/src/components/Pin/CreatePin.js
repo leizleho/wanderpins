f86efc96a546f5b42de2337c405fcfad7a1fe962
leizleho <ho.leizle@gmail.com>
2019-04-19 16:51:17 -0700

    uploads with cloudinary

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -16,9 +17,22 @@ const CreatePin = ({ classes }) => {
   const [image, setImage] = useState('');
   const [content, setContent] = useState('');
 
-  const handleSubmit = event => {
+  const handleImageUpload = async () => {
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'geopins');
+    data.append('cloud_name', 'lzlho');
+    const res = await axios.post(
+      'https://api.cloudinary.com/v1_1/lzlho/image/upload',
+      data
+    );
+    return res.data.url;
+  };
+
+  const handleSubmit = async event => {
     event.preventDefault();
-    console.log({ title, image, content });
+    const url = await handleImageUpload();
+    console.log({ title, image, url, content });
   };
 
   const handleDeleteDraft = () => {
