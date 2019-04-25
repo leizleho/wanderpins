a67141e213f4485a4e3918ff4d96a1924882e804
leizleho <ho.leizle@gmail.com>
2019-04-25 11:45:37 -0700

    built out components to create and display user comments

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -5,6 +5,8 @@ import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import FaceIcon from '@material-ui/icons/Face';
 import format from 'date-fns/format';
 
+import CreateComment from '../Comment/CreateComment';
+import Comments from '../Comment/Comments';
 import Context from '../../context';
 
 const PinContent = ({ classes }) => {
@@ -38,6 +40,10 @@ const PinContent = ({ classes }) => {
       <Typography variant="subtitle1" gutterBottom>
         {content}
       </Typography>
+
+      {/** Pin Comments */}
+      <CreateComment />
+      <Comments comments={comments} />
     </div>
   );
 };
