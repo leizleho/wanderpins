c4c51d9b355a0084488092f6aa2b69f6a1ee5c38
leizleho <ho.leizle@gmail.com>
2019-04-25 22:34:08 -0700

    setting up subscription on the backend

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,10 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, PubSub } = require('apollo-server');
 const Pin = require('./models/Pin');
+
+const pubsub = new PubSub();
+const PIN_ADDED = 'PIN_ADDED';
+const PIN_DELETED = 'PIN_DELETED';
+const PIN_UPDATED = 'PIN_UPDATED';
 
 const authenticated = next => (root, args, ctx, info) => {
   if (!ctx.currentUser) {
@@ -27,10 +32,12 @@ module.exports = {
         author: ctx.currentUser._id
       }).save();
       const pinAdded = await Pin.populate(newPin, 'author');
+      pubsub.publish(PIN_ADDED, { pinAdded });
       return pinAdded;
     }),
     deletePin: authenticated(async (root, args, ctx) => {
       const pinDeleted = await Pin.findOneAndDelete({ _id: args.pinId }).exec();
+      pubsub.publish(PIN_DELETED, { pinDeleted });
       return pinDeleted;
     }),
     createComment: authenticated(async (root, args, ctx) => {
@@ -42,7 +49,19 @@ module.exports = {
       )
         .populate('author')
         .populate('comments.author');
+      pubsub.publish(PIN_UPDATED, { pinUpdated });
       return pinUpdated;
     })
+  },
+  Subscription: {
+    pinAdded: {
+      subscribe: () => pubsub.asyncIterator(PIN_ADDED)
+    },
+    pinDeleted: {
+      subscribe: () => pubsub.asyncIterator(PIN_DELETED)
+    },
+    pinUpdated: {
+      subscribe: () => pubsub.asyncIterator(PIN_UPDATED)
+    }
   }
 };
--- typeDefs.js
+++ typeDefs.js
@@ -43,4 +43,10 @@ module.exports = gql`
     deletePin(pinId: ID!): Pin
     createComment(pinId: ID!, text: String!): Pin
   }
+
+  type Subscription {
+    pinAdded: Pin
+    pinDeleted: Pin
+    pinUpdated: Pin
+  }
 `;
